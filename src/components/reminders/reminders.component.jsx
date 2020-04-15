f3d2f4db62e75c0fea2489abc442d2638b18904b
hossamabdulmagid <hosamabdulmaged@gmail.com>
2020-04-16 00:12:48 +0200

    adding Reminder Component

--- src/components/reminders/reminders.component.jsx
+++ src/components/reminders/reminders.component.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
 
-
+import { H2, Span, LinkOption } from './reminders.styles'
 const Reminders = () => {
     return (
-        <div>Reminders</div>
+        <div>
+            <H2> Reminders</H2>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-1">
+                        <Span><i class="fa fa-pencil" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-usd" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-pencil" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-smile-o" aria-hidden="true"></i> </Span>
+                        <Span><i class="fa fa-heartbeat" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-pencil" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-pencil" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-file-o" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-credit-card-alt" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-credit-card-alt" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-pencil" aria-hidden="true"></i></Span>
+                    </div>
+
+                    <div className="col-8">
+                        <Span>170 clients or contacts in your account may be duplicates.your account will continue operating as it has been.
+                          <LinkOption> Review now </LinkOption> </Span>
+                        <Span> You have 283 clients with a balance due</Span>
+                        <Span>You have 4 emails that could not be delivered.</Span>
+                        <Span>You have not done a Mental status exam for these 103 clients.</Span>
+                        <Span> You have not done a Diagnosis and Treatment plan for these 99 clients.</Span>
+                        <Span>You have 42 clients that did not complete document requests</Span>
+                        <Span>You have 45 insurance payments with unallocated amounts</Span>
+                        <Span>You have 4 appointments with missing notes</Span>
+                        <Span>Client Makenna E. Murphy has an expired credit card </Span>
+                        <Span>Client Pickle Ploense has an expired credit card</Span>
+                        <Span>You have 13 denied claims</Span>
+                    </div>
+                    <div className="col-3">
+                        <Span><i class="fa fa-times" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-chevron-down" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-chevron-down" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-chevron-down" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-chevron-down" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-chevron-down" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-chevron-down" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-chevron-down" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-chevron-down" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-times" aria-hidden="true"></i></Span>
+                        <Span><i class="fa fa-times" aria-hidden="true"></i></Span>
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/components/reminders/reminders.styles.jsx
+++ src/components/reminders/reminders.styles.jsx
@@ -1,3 +1,32 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+export const H2 = styled.h2`
+font-size: 20px;
+font-weight: 400;
+line-height: 37px;
+color: #333;
+margin-top:13px;
+`;
+
+
+export const Span = styled.span`
+display:block;
+font-size:13px;
+
+padding-bottom:25px;
+`;
+
+export const SPAN = styled.span`
+font-size:13px;
+display:block;
+margin-top: -21px;
+
+`;
+
+export const Col = styled.div`
+border-bottom:1px solid gray;
+`;
+export const LinkOption = styled(Link)`
+padding:0px;
+`;
